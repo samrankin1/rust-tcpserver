5474da26b9ad40f195f7fef3804f034452a3a919
Sam Rankin <sam.rankin@me.com>
2017-05-28 11:20:40 -0400

    logically reorder cryptonet source

--- cryptonet/src/lib.rs
+++ cryptonet/src/lib.rs
@@ -16,81 +16,6 @@ use ring::{agreement, rand, pbkdf2, aead};
 use ring::rand::SecureRandom;
 use ring::agreement::EphemeralPrivateKey;
 
-fn net_encode_u64(data: u64) -> Vec<u8> {
-	let mut bytes: [u8; 8] = [0; 8]; // 64 bits = 8 bytes
-	NetworkEndian::write_u64(&mut bytes, data);
-
-	let mut result: Vec<u8> = Vec::with_capacity(8);
-	for byte in &bytes {
-		result.push(*byte);
-	}
-
-	result
-}
-
-fn net_decode_u64(encoded: &[u8]) -> u64 { // always a u64 for maximum compatibility
-	Cursor::new(encoded).read_u64::<NetworkEndian>().unwrap()
-}
-
-trait Netcode {
-
-	fn write_bytes(&mut self, bytes: &[u8]);
-	fn write_bytes_auto(&mut self, bytes: &[u8]);
-
-	fn read_bytes(&mut self, count: u64) -> Vec<u8>;
-	fn read_bytes_auto(&mut self, max_count: u64) -> Vec<u8>;
-
-}
-
-impl Netcode for TcpStream {
-
-	fn write_bytes(&mut self, bytes: &[u8]) {
-		self.write_all(bytes)
-			.expect("failed to write bytes to stream");
-	}
-
-	fn write_bytes_auto(&mut self, bytes: &[u8]) {
-		let encoded_len: Vec<u8> = net_encode_u64(bytes.len() as u64);
-
-		// println!("len = {}", bytes.len());
-		// println!("length bytes: u64 = {:?}", encoded_len);
-		// println!("data: [u8; {}] = {:?}", bytes.len(), bytes);
-
-		self.write_bytes(&encoded_len);
-		self.write_bytes(bytes);
-	}
-
-	fn read_bytes(&mut self, count: u64) -> Vec<u8> {
-		// println!("len = {}", count);
-
-		let mut result: Vec<u8> = vec![0; count as usize]; // TODO: "count as usize" may be unsafe
-		self.read_exact(&mut result)
-			.expect("failed to read bytes");
-
-		// println!("bytes: [u8; {}] = {:?}", count, result);
-
-		result
-	}
-
-	fn read_bytes_auto(&mut self, max_count: u64) -> Vec<u8> {
-		let len_bytes: Vec<u8> = self.read_bytes(8);
-
-		// println!("length bytes: u64 = {:?}", len_bytes);
-
-		let len: u64 = net_decode_u64(&len_bytes);
-
-		// hard limit on memory allocated per read call (and packet size)
-		// prevents malicious or malformed packets from demanding large buffers
-		if len > max_count {
-			return Vec::new(); // return empty set of bytes, signaling failure
-		}
-
-		self.read_bytes(len)
-	}
-
-}
-
-
 static ECDH_CURVE: &ring::agreement::Algorithm = &agreement::X25519; // Curve25519, as described in RFC 7748
 const PUB_KEY_LEN: usize = 32; // X25519 takes 32 bytes as a key
 
@@ -180,6 +105,80 @@ impl AESPacket {
 			.expect("failed to decrypt bytes with AES!");
 
 		decrypted_output.to_vec()
+	}
+
+}
+
+fn net_encode_u64(data: u64) -> Vec<u8> {
+	let mut bytes: [u8; 8] = [0; 8]; // 64 bits = 8 bytes
+	NetworkEndian::write_u64(&mut bytes, data);
+
+	let mut result: Vec<u8> = Vec::with_capacity(8);
+	for byte in &bytes {
+		result.push(*byte);
+	}
+
+	result
+}
+
+fn net_decode_u64(encoded: &[u8]) -> u64 { // always a u64 for maximum compatibility
+	Cursor::new(encoded).read_u64::<NetworkEndian>().unwrap()
+}
+
+trait Netcode {
+
+	fn write_bytes(&mut self, bytes: &[u8]);
+	fn write_bytes_auto(&mut self, bytes: &[u8]);
+
+	fn read_bytes(&mut self, count: u64) -> Vec<u8>;
+	fn read_bytes_auto(&mut self, max_count: u64) -> Vec<u8>;
+
+}
+
+impl Netcode for TcpStream {
+
+	fn write_bytes(&mut self, bytes: &[u8]) {
+		self.write_all(bytes)
+			.expect("failed to write bytes to stream");
+	}
+
+	fn write_bytes_auto(&mut self, bytes: &[u8]) {
+		let encoded_len: Vec<u8> = net_encode_u64(bytes.len() as u64);
+
+		// println!("len = {}", bytes.len());
+		// println!("length bytes: u64 = {:?}", encoded_len);
+		// println!("data: [u8; {}] = {:?}", bytes.len(), bytes);
+
+		self.write_bytes(&encoded_len);
+		self.write_bytes(bytes);
+	}
+
+	fn read_bytes(&mut self, count: u64) -> Vec<u8> {
+		// println!("len = {}", count);
+
+		let mut result: Vec<u8> = vec![0; count as usize]; // TODO: "count as usize" may be unsafe
+		self.read_exact(&mut result)
+			.expect("failed to read bytes");
+
+		// println!("bytes: [u8; {}] = {:?}", count, result);
+
+		result
+	}
+
+	fn read_bytes_auto(&mut self, max_count: u64) -> Vec<u8> {
+		let len_bytes: Vec<u8> = self.read_bytes(8);
+
+		// println!("length bytes: u64 = {:?}", len_bytes);
+
+		let len: u64 = net_decode_u64(&len_bytes);
+
+		// hard limit on memory allocated per read call (and packet size)
+		// prevents malicious or malformed packets from demanding large buffers
+		if len > max_count {
+			return Vec::new(); // return empty set of bytes, signaling failure
+		}
+
+		self.read_bytes(len)
 	}
 
 }
